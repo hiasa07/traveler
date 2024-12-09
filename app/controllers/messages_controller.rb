82c250ceda3df02b19d21f90569d82cf253d9a97
hiasa07 <181676759+hiasa07@users.noreply.github.com>
2024-12-10 06:59:52 +0900

    コード修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,29 +1,34 @@
 class MessagesController < ApplicationController
-    before_action :set_user
+  before_action :set_user
   
-    def index
-      @messages = Message.where(sender: current_user, receiver: @user)
-                         .or(Message.where(sender: @user, receiver: current_user))
-                         .order(:created_at)
-      @new_message = Message.new
-    end
-  
-    def create
+  def index
+    @messages = Message.where(sender: current_user, receiver: @user)
+                       .or(Message.where(sender: @user, receiver: current_user))
+                       .order(:created_at)
+    @new_message = Message.new
+  end
+
+  def create
+    if @user.present?
       @message = current_user.sent_messages.build(receiver: @user, content: params[:message][:content])
-    
+      
       if @message.save
-        redirect_to user_messages_path(@message.sender_id), notice: "メッセージを送信しました！"
+        redirect_to user_messages_path(@user), notice: "メッセージを送信しました！"
       else
         @messages = Message.where(sender: current_user, receiver: @user)
                            .or(Message.where(sender: @user, receiver: current_user))
                            .order(:created_at)
-        flash.now[:alert] = @message.errors.full_messages.join(", ")
+        flash.now[:alert] = "メッセージの送信に失敗しました: " + @message.errors.full_messages.join(", ")
         render :index
       end
-    end    
-  
-    private
-    def set_user
-      @user = User.find(params[:user_id])
+    else
+      redirect_to root_path, alert: "相手ユーザーが見つかりませんでした。" and return
     end
-  end  
+  end    
+
+  private
+  def set_user
+    @user = User.find_by(id: params[:user_id])
+    redirect_to root_path, alert: "ユーザーが見つかりませんでした。" unless @user
+  end
+end
